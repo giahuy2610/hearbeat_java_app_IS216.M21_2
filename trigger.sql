321150de5a77fdfd72108138a9fafd533f960a21
giahuy2610 <giahuytrinh.26102002@gmail.com>
2022-04-26 22:40:04 +0700

    edit procedures

--- trigger.sql
+++ trigger.sql
@@ -18,12 +18,21 @@ END;
 /
 
 --tôi tạo thêm trigger này để lấy ngày tạo POST tự động
-CREATE OR REPLACE TRIGGER TRIGGER_AUTOINCRE_TB_POST_CREATEDON
-BEFORE INSERT ON
-TB_POST
-REFERENCING NEW AS NEW
-    FOR EACH ROW BEGIN SELECT
-    CURRENT_DATE INTO :NEW.CREATEDON
-    FROM DUAL;
+CREATE OR REPLACE TRIGGER trigger_default_value_post BEFORE
+    INSERT ON tb_post
+    REFERENCING
+        NEW AS new
+    FOR EACH ROW
+BEGIN
+    SELECT
+        current_date
+    INTO :new.createdon
+    FROM
+        dual;
+
+    :new.partnerid := NULL;
+    :new.updatedon := NULL;
+    :new.imagepath := NULL;
+    :new.statusid := 1;
+    :new.isdeleted := 0;
 END;
-/
