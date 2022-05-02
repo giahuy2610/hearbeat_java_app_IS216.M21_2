28cc5492d1ce1bf1161ef1ae570aa26e2c7873f0
giahuy2610 <giahuytrinh.26102002@gmail.com>
2022-05-02 09:46:22 +0700

    add tb_statement

--- heartbeat/src/DangNhap.java
+++ heartbeat/src/DangNhap.java
@@ -2,4 +2,7 @@ import javax.swing.*;
 
 public class DangNhap {
     private JPanel panel1;
+    private JButton button1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
 }
